480df38012452f8a47147da659c999bdec659582
vinh <ntvinh.11586@gmail.com>
2016-05-31 08:15:50 +0700

    v0.1.3  config layout style, create change Information

---
+++ app/src/main/java/com/example/vinh/booklinkers/YourInformationEditing.java
@@ -0,0 +1,13 @@
+package com.example.vinh.booklinkers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YourInformationEditing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_information_editing);
+    }
+}
