efd4096286448facdd61388db01585b64683a807
vinh <ntvinh.11586@gmail.com>
2016-05-31 20:23:57 +0700

    v0.3.6  fix some bugs on your information activity and add nav bar

--- app/src/main/java/com/example/vinh/booklinkers/YourInformationActivity.java
+++ app/src/main/java/com/example/vinh/booklinkers/YourInformationActivity.java
@@ -1,11 +1,19 @@
 package com.example.vinh.booklinkers;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.vinh.GlobalObject.ConnectingServerData;
 import com.firebase.client.DataSnapshot;
@@ -15,7 +23,8 @@ import com.firebase.client.ValueEventListener;
 
 import org.w3c.dom.Text;
 
-public class YourInformationActivity extends AppCompatActivity {
+public class YourInformationActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     private Button btnChangeInformation;
 
@@ -32,6 +41,20 @@ public class YourInformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_personal_information);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         tvName = (TextView)findViewById(R.id.text_name);
         tvUsername = (TextView)findViewById(R.id.text_username);
@@ -54,9 +77,9 @@ public class YourInformationActivity extends AppCompatActivity {
                 tvUsername.setText("Username            " + snapshot.child("username").getValue().toString());
                 tvEmail.setText("Email          " + snapshot.child("email").getValue().toString());
 
-                tvBirthday.setText("Birthday            " + snapshot.child("name").getValue().toString());
-                tvPhone.setText("Phone          " + snapshot.child("name").getValue().toString());
-                tvAddress.setText("Address         "+ snapshot.child("name").getValue().toString());
+                tvBirthday.setText("Birthday            " + snapshot.child("birthday").getValue().toString());
+                tvPhone.setText("Phone          " + snapshot.child("phone").getValue().toString());
+                tvAddress.setText("Address          " + snapshot.child("address").getValue().toString());
             }
 
             @Override
@@ -73,5 +96,63 @@ public class YourInformationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            return true;
+        } else if (id == R.id.action_home) {
+            Intent intent = new Intent(YourInformationActivity.this, MainActivity.class);
+            finish();
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_log_out) {
+            Intent intent = new Intent(YourInformationActivity.this, LoginActivity.class);
+            finish();
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_have_book) {
+            Toast.makeText(getApplicationContext(), "ok", Toast.LENGTH_SHORT).show();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
