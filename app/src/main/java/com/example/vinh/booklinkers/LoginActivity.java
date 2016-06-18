4e73cf012e1c4c9cd990e5b232c62df4f2a736f9
The Vinh Nguyen <ntvinh.11586@gmail.com>
2016-06-18 16:33:48 +0700

    v0.5.6 add remember login

--- app/src/main/java/com/example/vinh/booklinkers/LoginActivity.java
+++ app/src/main/java/com/example/vinh/booklinkers/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.vinh.booklinkers;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -26,6 +28,11 @@ public class LoginActivity extends AppCompatActivity {
     boolean bUsername = false;
     boolean bPassword = false;
     Firebase myFirebaseRef;
+    private CheckBox cbRememberPassword;
+
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    private Boolean saveLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +45,19 @@ public class LoginActivity extends AppCompatActivity {
         etPassword = (EditText)findViewById(R.id.edit_password);
         btnLogin = (Button)findViewById(R.id.button_login);
         btnCreateNewAccount = (Button)findViewById(R.id.button_create_account);
+        cbRememberPassword = (CheckBox)findViewById(R.id.check_save_login);
+
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+
+        loginPrefsEditor = loginPreferences.edit();
+
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+
+        if (saveLogin == true) {
+            etUsername.setText(loginPreferences.getString("username", ""));
+            etPassword.setText(loginPreferences.getString("password", ""));
+            cbRememberPassword.setChecked(true);
+        }
 
         Firebase.setAndroidContext(LoginActivity.this);
         myFirebaseRef = new Firebase("https://booklinkers-db.firebaseio.com/");
@@ -47,6 +67,17 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 username = etUsername.getText().toString();
                 password = etPassword.getText().toString();
+
+                if (cbRememberPassword.isChecked()) {
+                    loginPrefsEditor.putBoolean("saveLogin", true);
+                    loginPrefsEditor.putString("username", username);
+                    loginPrefsEditor.putString("password", password);
+                    loginPrefsEditor.commit();
+                } else {
+                    loginPrefsEditor.clear();
+                    loginPrefsEditor.commit();
+                }
+
 
                 myFirebaseRef.addValueEventListener(new ValueEventListener() {
                     @Override
