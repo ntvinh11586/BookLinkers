36b233cb67c6fbfbffe39a5e1a376e26381e7473
ntvinh11586 <ntvinh.11586@gmail.com>
2016-07-06 01:08:06 +0700

    add test firebase auth

--- app/src/main/java/com/example/vinh/booklinkers/TestActivity.java
+++ app/src/main/java/com/example/vinh/booklinkers/TestActivity.java
@@ -5,22 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestActivity extends AppCompatActivity {
 
     private Button btnMessage;
+    private Firebase myFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_activty);
 
+        Firebase.setAndroidContext(TestActivity.this);
+        myFirebaseRef = new Firebase("https://booklinkers-database.firebaseio.com/");
+
+
+
         btnMessage = (Button)findViewById(R.id.button);
 
         btnMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(TestActivity.this, BooksActivity.class));
+                myFirebaseRef.createUser("bobtony@firebase.com", "123456", new Firebase.ValueResultHandler<Map<String, Object>>() {
+                    @Override
+                    public void onSuccess(Map<String, Object> result) {
+                        Toast.makeText(TestActivity.this, (String)result.get("uid"), Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public void onError(FirebaseError firebaseError) {
+                        Toast.makeText(TestActivity.this, firebaseError.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
